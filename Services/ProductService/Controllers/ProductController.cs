20f8261e095c270bc83e8f8ace48db4380a52604
MarcinKruszynski <martin.kruszynski@gmail.com>
2018-04-16 02:03:18 +0200

    Api gateway (Ocelot) routing

---
+++ Services/ProductService/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductService.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class ProductController: Controller
+    {        
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "DM 4", "Drużyna mistrzów 5" };
+        }
+        
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"DM {id}";
+        }       
+    }
+}

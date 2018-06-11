6ac90c3e446cc4d1838d09c5ae6556db9311cc2e
MarcinKruszynski <martin.kruszynski@gmail.com>
2018-06-11 02:01:23 +0200

    booking service - model - first step

---
+++ Services/BookingService/Model/Booking.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace BookingService.Model
+{
+    public class Booking
+    {
+        public int Id { get; private set; }
+
+        private DateTime _date;
+
+        private int? _buyerId;
+
+        public BookingStatus Status { get; private set; }
+        private int _statusId;
+
+        public int ProductId { get; private set; }
+
+        private string _productName;
+        private decimal _unitPrice;
+        private int _units;
+
+        public Booking(int productId, string productName, decimal unitPrice, int units = 1, int? buyerId = null)
+        {
+            _buyerId = buyerId;
+            _statusId = BookingStatus.Submitted.Id;
+            _date = DateTime.UtcNow;
+
+            ProductId = productId;
+
+            _productName = productName;
+            _unitPrice = unitPrice;            
+            _units = units;
+        }
+
+        public void SetBuyerId(int id)
+        {
+            _buyerId = id;
+        }
+
+        public int GetUnits() => _units;        
+
+        public decimal GetUnitPrice() => _unitPrice;        
+
+        public string GetOrderItemProductName() => _productName;
+
+
+        public void SetCheckingAvailability()
+        {
+            _statusId = BookingStatus.CheckingAvailability.Id;
+        }
+
+        public void SetStockConfirmedStatus()
+        {
+            _statusId = BookingStatus.StockConfirmed.Id;            
+        }
+
+        public void SetPaidStatus()
+        {
+            if (_statusId != BookingStatus.StockConfirmed.Id)
+            {
+                throw new Exception($"Not possible to change booking status from {Status.Name} to {BookingStatus.Paid.Name}.");
+            }
+
+            _statusId = BookingStatus.Paid.Id;            
+        }
+
+        public void SetCancelledStatus()
+        {
+            if (_statusId == BookingStatus.Paid.Id)
+            {
+                throw new Exception($"Not possible to change booking status from {Status.Name} to {BookingStatus.Cancelled.Name}.");
+            }
+
+            _statusId = BookingStatus.Cancelled.Id;            
+        }
+    }
+}
---
+++ Services/BookingService/Model/BookingStatus.cs
@@ -0,0 +1,53 @@
+﻿using BookingService.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookingService.Model
+{
+    public class BookingStatus: Enumeration
+    {
+        public static BookingStatus Submitted = new BookingStatus(1, nameof(Submitted).ToLowerInvariant());
+        public static BookingStatus CheckingAvailability = new BookingStatus(2, nameof(CheckingAvailability).ToLowerInvariant());
+        public static BookingStatus StockConfirmed = new BookingStatus(3, nameof(StockConfirmed).ToLowerInvariant());
+        public static BookingStatus Paid = new BookingStatus(4, nameof(Paid).ToLowerInvariant());        
+        public static BookingStatus Cancelled = new BookingStatus(6, nameof(Cancelled).ToLowerInvariant());
+
+        protected BookingStatus()
+        {
+        }
+
+        public BookingStatus(int id, string name)
+            : base(id, name)
+        {
+        }
+
+        public static IEnumerable<BookingStatus> List() =>
+            new[] { Submitted, CheckingAvailability, StockConfirmed, Paid, Cancelled };
+
+        public static BookingStatus FromName(string name)
+        {
+            var state = List()
+                .SingleOrDefault(s => String.Equals(s.Name, name, StringComparison.CurrentCultureIgnoreCase));
+
+            if (state == null)
+            {
+                throw new ArgumentException($"Possible values for BookingStatus: {String.Join(",", List().Select(s => s.Name))}");
+            }
+
+            return state;
+        }
+
+        public static BookingStatus From(int id)
+        {
+            var state = List().SingleOrDefault(s => s.Id == id);
+
+            if (state == null)
+            {
+                throw new ArgumentException($"Possible values for BookingStatus: {String.Join(",", List().Select(s => s.Name))}");
+            }
+
+            return state;
+        }
+    }
+}

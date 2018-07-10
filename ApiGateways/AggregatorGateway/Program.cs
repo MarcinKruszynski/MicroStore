a192a9a0f3759ab0415709ea3da6e30d5249b819
MarcinKruszynski <martin.kruszynski@gmail.com>
2018-07-10 08:16:02 +0200

    aggregator gateway - step 1.1

--- ApiGateways/AggregatorGateway/Program.cs
+++ ApiGateways/AggregatorGateway/Program.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AggregatorGateway
 {
@@ -14,11 +10,17 @@ namespace AggregatorGateway
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+            BuildWebHost(args).Run();
+        }        
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var builder = WebHost.CreateDefaultBuilder(args);
+            builder.ConfigureServices(s => s.AddSingleton(builder))
+                .ConfigureAppConfiguration(ic => ic.AddJsonFile(Path.Combine("configuration", "configuration.json")))
                 .UseStartup<Startup>();
+            var host = builder.Build();
+            return host;
+        }
     }
 }

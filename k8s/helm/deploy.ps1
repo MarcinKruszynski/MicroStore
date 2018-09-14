69308d9a870aeae68d3214d4099b60a3c421d5d9
MarcinKruszynski <martin.kruszynski@gmail.com>
2018-09-14 19:59:10 +0200

    add helm webstatus deployment

--- k8s/helm/deploy.ps1
+++ k8s/helm/deploy.ps1
@@ -57,7 +57,7 @@ $infras.Add($newRec7)
 # $newRec9 = [InfraRecord] @{ Name = 'kibana'; Image = 'stable/kibana'}
 # $infras.Add($newRec9)
 
-$charts = ("identityservice", "apigateway", "bookingagg", "bookingservice", "paymentservice", "notificationservice", "webapp")
+$charts = ("identityservice", "apigateway", "bookingagg", "bookingservice", "paymentservice", "notificationservice", "webapp", "webstatus")
 # "apigateway", "bookingagg", "productservice", "bookingservice", "paymentservice", "notificationservice", "webapp", "webstatus")
 
 if ($deployInfrastructure) {
